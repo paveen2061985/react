b5244288491cf047bd0fedffa6891fe19e9f6ce9
Praveen Sanikommu <praveenreddy206@gmail.com>
2023-11-23 17:53:16 +0530

    Adding Parcel bundler

--- App.js
+++ App.js
@@ -1,29 +1,13 @@
-/*const heading = React.createElement(
-    "h1",
-    {id:'heading'},
-    "Welcome to React from React"
-);*/
-/*
-*
-* <div id ="parent">
-*   <div id="child1">
-*     <h1>I am h1 tag</h1>
-*     <h2> I am h2 tag</h2>
-*   </div>
-*   <div id="child2">
-*     <h1>I am h1 tag</h1>
-*     <h2> I am h2 tag</h2>
-*   </div>
-* </div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id: 'parent'}, [
     React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h1", {}, "Welcome to React ✋"),
         React.createElement("h2", {}, "I am h2 tag")
     ]),
     React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h1", {}, "React is Awesome Framework 😍"),
         React.createElement("h2", {}, "I am h2 tag")
     ])
 ]);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # React Complete Code
+
+# Parcel
+=> Parcel do Dev Build
+=> Local Server
+=> Parcel does HMR(Hot Module Replacement)
+=> Parcel uses File Watching Algorithm written in C++
+=> Caching - Due to Caching, faster builds
+=> Image Optimization
+=> Bundling and minification
+=> Compress the files
+=> Consistent Hashing
+=> Code splitting
+=> Differential bundling to support older browsers
+=> Error handling
+=> HTTPS
+=> Diagnostics
+=> Tree shaking - remove unused code for us.
+=> Different dev and prod bundles.
+
+
+
