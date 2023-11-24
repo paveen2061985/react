a7c97e872a1df45b07e22e4c7414263900251b24
Praveen Sanikommu <praveenreddy206@gmail.com>
2023-11-24 18:05:15 +0530

    end of foundation layout

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX -> JSX is a Javascript extension to make developer life easy while developing React Apps.
-// JSX is not writing HTML in Javascript but It is HTML like syntax.
-// JSX => React.createElement => JS (Object) => HTMLElement(render)
-const jsxHeading = (
+const Title = () => (
         <h1 className="head" tabIndex="1">
             Welcome to React using JSX 🚀
         </h1>
     );
 
+const title = (
+        <h1 className="head2" tabIndex="1">
+            React Element using JSX 🚀
+        </h1>
+);
+
+// React Component
+// There two ways to create component - OLD - Uses javascript classes
+// Class based component and functional components - NEW - Uses javascript functions
+// React functional Component is just a javascript function
+// Component composition
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        {title}
+        <h1 className="heading">It is React Functional Component 🚀</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
