f6abbbef483b1a1e996961f3290725b497cc5206
Praveen Sanikommu <praveenreddy206@gmail.com>
2023-11-24 17:00:20 +0530

    Changing to JSX

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: 'parent'}, [
-    React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "Welcome to React 🤘"),
-        React.createElement("h2", {}, "I am h2 tag")
-    ]),
-    React.createElement("div", {id:"child2"}, [
-        React.createElement("h1", {}, "React is Awesome Framework 😍"),
-        React.createElement("h2", {}, "I am h2 tag")
-    ])
-]);
+// JSX -> JSX is a Javascript extension to make developer life easy while developing React Apps.
+// JSX is not writing HTML in Javascript but It is HTML like syntax.
+// JSX => React.createElement => JS (Object) => HTMLElement(render)
+const jsxHeading = (
+        <h1 className="head" tabIndex="1">
+            Welcome to React using JSX 🚀
+        </h1>
+    );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(jsxHeading);
